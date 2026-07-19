71f3295f689efc6da8178bba8132a734519579ee
HaJinhyeok <hjhmoon61@naver.com>
2026-07-19 21:57:50 +0900

    [HA] 미사용 분할점 계산 함수 제거
    - 호출처가 없는 CalculateDivisionPoints 선언과 정의 제거
    - 불필요한 vector 반환 API와 유지보수 대상 축소
    - 현재 소스에 관련 참조가 남지 않은 것을 확인
    - Debug x86 빌드 경고 0개, 오류 0개 검증
    - P1-07 완료 상태와 삭제 근거 문서화

--- maze_function.cpp
+++ maze_function.cpp
@@ -4,15 +4,6 @@ D3DXVECTOR3 CalculateMidPoint(D3DXVECTOR3 pos1, D3DXVECTOR3 pos2)
 {
     return D3DXVECTOR3((pos1 + pos2).x / 2, (pos1 + pos2).y / 2, (pos1 + pos2).z / 2);
 }
-vector<D3DXVECTOR3> CalculateDivisionPoints(D3DXVECTOR3 pos1, D3DXVECTOR3 pos2, int div)
-{
-    vector<D3DXVECTOR3> points;
-    for (int i = 1;i <= div - 1;i++)
-    {
-        points.push_back(D3DXVECTOR3((pos1.x * (div - i) + pos2.x * i) / div, (pos1.y * (div - i) + pos2.y * i) / div, (pos1.z * (div - i) + pos2.z * i) / div));
-    }
-    return points;
-}
 
 // 몇 번째 맵을 만들건지 주어지면, (맵 외곽을 제외한) 맵 내부를 구성하는 벽들의 vertex 좌표를 생성
 // 일단 맵은 하나만...
--- maze_function.h
+++ maze_function.h
@@ -4,6 +4,5 @@
 #include "CSetting.h"
 
 D3DXVECTOR3 CalculateMidPoint(D3DXVECTOR3 pos1, D3DXVECTOR3 pos2);
-vector<D3DXVECTOR3> CalculateDivisionPoints(D3DXVECTOR3 pos1, D3DXVECTOR3 pos2, int div);
 VOID GenerateMazeWall(int nMapNumber, CUSTOMVERTEX (*Maze)[20], vector<CNotice>* notice, CExit* exit);
 VOID MakeWallBlock(CUSTOMVERTEX* Block, D3DXVECTOR3 position);
